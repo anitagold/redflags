65f1193f4ada3c049ebd6521e11bbbca5558e344
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2016-10-13 11:28:04 +0200

    Spring Boot upgrade to 1.4.1

--- redflags-web/src/main/java/hu/petabyte/redflags/web/App.java
+++ redflags-web/src/main/java/hu/petabyte/redflags/web/App.java
@@ -19,8 +19,6 @@ import java.net.URISyntaxException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -30,16 +28,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAutoConfiguration
 @EnableScheduling
 @ComponentScan
-public class App extends SpringBootServletInitializer {
+public class App /* extends SpringBootServletInitializer */{
 
 	public static void main(String[] args) throws URISyntaxException {
 		SpringApplication.run(App.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder application) {
-		return application.sources(App.class);
-	}
+	/*
+	 * @Override protected SpringApplicationBuilder configure(
+	 * SpringApplicationBuilder application) { return
+	 * application.sources(App.class); }
+	 */
 
 }
