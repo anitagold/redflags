6ae1a764562e27b05101412ccea42296ab6b71ca
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2017-02-06 10:19:52 +0100

    Optimized helper tables SQL script

--- redflags-helper-tables.sql
+++ redflags-helper-tables.sql
@@ -16,6 +16,19 @@
 
 -- NOTICES
 
+drop table if exists rfwl_temp_dt;
+create table rfwl_temp_dt as
+select r.relationLeftId noticeId, r.relationRightId 'typeId', dt.name 'typeName'
+from te_relationdescriptor r
+inner join te_datatype dt on dt.id = r.relationRightId and r.additonalInfo = "documentType";
+alter table rfwl_temp_dt change column `noticeId` `noticeId` varchar(200) not null, add primary key (`noticeId`);
+
+drop table if exists rfwl_temp_co;
+create table rfwl_temp_co as
+select r.relationLeftId noticeId, r.relationRightId orgId, o.name orgName
+from te_relationdescriptor r inner join te_organization o on o.id = r.relationRightId and r.additonalInfo = "contractingOrg";
+alter table rfwl_temp_co change column `noticeId` `noticeId` varchar(200) not null, add primary key (`noticeId`);
+
 drop table if exists rfwl_temp;
 create table rfwl_temp as
 	select
@@ -28,45 +41,26 @@ create table rfwl_temp as
 		c.orgId 'contractingOrgId',
 		c.orgName 'contractingOrgName',
 		o.contractTitle 'title',
-		o.estimated 'estimated',
-		o.estimatedCurr 'estimatedCurr',
-		o.total 'total',
-		o.totalCurr 'totalCurr',
+		o.estimatedValue 'estimated',
+		o.estimatedValueCurr 'estimatedCurr',
+		o.totalFinalValue 'total',
+		o.totalFinalValueCurr 'totalCurr',
 		fcs.fc 'flagCount'
 	from
 		(select id, documentFamilyId, tedUrl from te_notice order by noticeYear desc, noticeNumber desc) n
-		left join (
-			select id, publicationDate
-			from te_data
-			) d on n.id = d.id
-		left join (
-			select r.nid noticeId, r.tid 'typeId', dt.name 'typeName'
-			from
-				(select relationLeftId nid, relationRightId tid from te_relationdescriptor where additonalInfo = "documentType") r
-				inner join te_datatype dt on dt.id = r.tid
-			) t on n.id = t.noticeId
-		left join (
-			select r.relationLeftId noticeId, r.relationRightId orgId, o.name orgName
-			from te_relationdescriptor r inner join te_organization o on o.id = r.relationRightId
-			where r.additonalInfo = "contractingOrg"
-			) c on n.id = c.noticeId
-		left join (
-			select
-				noticeId,
-				group_concat(DISTINCT contractTitle SEPARATOR '; ') 'contractTitle',
-				sum(estimatedValue) 'estimated',
-				group_concat(DISTINCT estimatedValueCurr SEPARATOR '; ') 'estimatedCurr',
-				sum(totalFinalValue) 'total',
-				group_concat(DISTINCT totalFinalValueCurr SEPARATOR '; ') 'totalCurr'
-			from te_objofthecontract group by noticeId
-			) o on n.id = o.noticeId
+		left join te_data d on n.id = d.id
+		left join rfwl_temp_dt t on n.id = t.noticeId
+		left join rfwl_temp_co c on n.id = c.noticeId
+		left join te_objofthecontract o on o.id = CONCAT(n.id, '-O-1')
 		left join (
 			select noticeId, count(*) fc from te_flag group by noticeId
-			) fcs on n.id = fcs.noticeId
-; -- 112 sec
+		) fcs on n.id = fcs.noticeId
+;
 alter table rfwl_temp add primary key (id), add index (flagCount);
 drop table if exists rfwl_notices;
 rename table rfwl_temp to rfwl_notices;
+drop table if exists rfwl_temp_dt;
+drop table if exists rfwl_temp_co;
 
 -- ORGANIZATIONS
 
@@ -126,7 +120,11 @@ create table rfwl_temp as
 		where additonalInfo = "winnerOrg") r
 		inner join te_organization o on o.id = r.oid
 ; -- 0.6 sec
-alter table rfwl_temp change column `noticeId` `noticeId` VARCHAR(200) NOT NULL , add primary key (`noticeId`);
+alter table rfwl_temp
+	change column `noticeId` `noticeId` varchar(20) not null,
+	change column `winnerOrgId` `winnerOrgId` varchar(200) not null,
+	change column `winnerOrgName` `winnerOrgName` longtext not null,
+	add index `id` (`noticeId` asc);
 drop table if exists rfwl_winners;
 rename table rfwl_temp to rfwl_winners;
 
